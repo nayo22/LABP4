602ed15429c4e2b7ea1b8668ddf20d50946117a0
Nayo <skynet8252@gmail.com>
2025-05-02 16:56:55 -0500

    feat(data): agregadas imágenes a los peleadores en fightData para visualización con estilos

--- src/assets/fightData.ts
+++ src/assets/fightData.ts
@@ -1,16 +1,68 @@
+
+
 export interface Fight {
   id: string;
   a: string;
   b: string;
+  imgA: string;
+  imgB: string;
 }
 
 export const fights: Fight[] = [
-  { id: 'fight1', a: 'Rubius', b: 'Ibai' },
-  { id: 'fight2', a: 'Auron', b: 'Reborn' },
-  { id: 'fight3', a: 'Grefg', b: 'WestCol' },
-  { id: 'fight4', a: 'Rivers', b: 'La Rivers de verdad' },
-  { id: 'fight5', a: 'Ampeter', b: 'Coscu' },
-  { id: 'fight6', a: 'DJMariio', b: 'Carola' },
-  { id: 'fight7', a: 'Spursito', b: 'Fernanfloo' },
-  { id: 'fight8', a: 'Jagger', b: 'Zeling' }
+  {
+    id: 'fight1',
+    a: 'Rubius',
+    b: 'Ibai',
+    imgA: 'img/Rubius.png',
+    imgB: 'img/Ibai.png'
+  },
+  {
+    id: 'fight2',
+    a: 'Auron',
+    b: 'Reborn',
+    imgA: 'img/Auron.png',
+    imgB: 'img/Reborn.png'
+  },
+  {
+    id: 'fight3',
+    a: 'Grefg',
+    b: 'WestCol',
+    imgA: 'img/Grefg.png',
+    imgB: 'img/Westcol.png'
+  },
+  {
+    id: 'fight4',
+    a: 'Rivers',
+    b: 'La Rivers de verdad',
+    imgA: 'img/Rivers.png',
+    imgB: 'img/Larivers.png'
+  },
+  {
+    id: 'fight5',
+    a: 'Ampeter',
+    b: 'Coscu',
+    imgA: 'img/Ampeter.png',
+    imgB: 'img/Coscu.png'
+  },
+  {
+    id: 'fight6',
+    a: 'DJMariio',
+    b: 'Carola',
+    imgA: 'img/DJmario.png',
+    imgB: 'img/Carola.png'
+  },
+  {
+    id: 'fight7',
+    a: 'Spursito',
+    b: 'Fernanfloo',
+    imgA: 'img/Spursito.png',
+    imgB: 'img/Fernanfloo.png'
+  },
+  {
+    id: 'fight8',
+    a: 'Jagger',
+    b: 'Zeling',
+    imgA: 'img/Jagger.png',
+    imgB: 'img/Zeling.png'
+  }
 ];
